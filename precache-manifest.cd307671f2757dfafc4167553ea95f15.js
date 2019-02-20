9da34344c2991fcbd98006dde61eebd0a2e13d28
Tan Phan <duytan.phanle@gmail.com>
2019-02-20 23:28:09 +0700

    Updates

---
+++ precache-manifest.cd307671f2757dfafc4167553ea95f15.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "928cd53f19420e58b262",
+    "url": "/react-firebase-clientpanel/static/js/runtime~main.928cd53f.js"
+  },
+  {
+    "revision": "41dd3f2b55965e428dde",
+    "url": "/react-firebase-clientpanel/static/js/main.41dd3f2b.chunk.js"
+  },
+  {
+    "revision": "b4d28fd1e146e200345d",
+    "url": "/react-firebase-clientpanel/static/js/2.b4d28fd1.chunk.js"
+  },
+  {
+    "revision": "6c012e09e32f959f1d8043c84365ee24",
+    "url": "/react-firebase-clientpanel/index.html"
+  }
+];
