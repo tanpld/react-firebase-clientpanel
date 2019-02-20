40ccc075e058c1600089932730ddf54df515401f
Tan Phan <duytan.phanle@gmail.com>
2019-02-20 11:58:06 +0700

    Add NavBar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react'
-import './App.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+import AppNavBar from './components/layout/AppNavBar'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>Test</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <AppNavBar />
+          <div className="container">test</div>
+        </div>
+      </Router>
     )
   }
 }
