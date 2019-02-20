0b373f1f1e3654cb6dd1f8e0165d9b6e3b5081c1
Tan Phan <duytan.phanle@gmail.com>
2019-02-20 22:10:05 +0700

    Persist Settings To LocalStorage

--- src/reducers/settingsReducer.js
+++ src/reducers/settingsReducer.js
@@ -4,28 +4,24 @@ import {
   ALLOW_REGISTRATION
 } from '../actions/types'
 
-const initialState = {
-  disableBalanceOnAdd: true,
-  disableBalanceOnEdit: false,
-  allowRegistration: false
-}
+const initialState = {}
 
 export default function(state = initialState, action) {
   switch (action.type) {
     case DISABLE_BALANCE_ON_ADD:
       return {
         ...state,
-        disableBalanceOnAdd: !state.disableBalanceOnAdd
+        disableBalanceOnAdd: action.payload
       }
     case DISABLE_BALANCE_ON_EDIT:
       return {
         ...state,
-        disableBalanceOnEdit: !state.disableBalanceOnEdit
+        disableBalanceOnEdit: action.payload
       }
     case ALLOW_REGISTRATION:
       return {
         ...state,
-        allowRegistration: !state.allowRegistration
+        allowRegistration: action.payload
       }
     default:
       return state
